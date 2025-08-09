6f1d5af6fd4d197979a9ee037b056503c5652fe6
Roberto8792 <robertotulli@ymail.com>
2025-08-09 06:37:31 -0400

    up

--- PYTHON/connessioni_rete.py
+++ PYTHON/connessioni_rete.py
@@ -13,7 +13,7 @@ s.bind ((SRV_ADDR, SRV_PORT))
 
 #con il modulo s.liesrt configuro il socket per ascoltare sulla coppia indicata (ip : porta)
 #indicando tra le parentesi il numero massimo di connessioni
-s.liste (1)
+s.listen (1)
 
 #faccio un print per specificare che sta in attesa di connessioni
 print("Server partito! In attesa di connessione...")
@@ -28,7 +28,7 @@ print("Client connesso con l'indirizzo:", address)
 while True:                         #ciclo infinito
     data = connection.recv(1024)    #per ricevere i dati dal client
     if not data : break             #se il client chiude connessione esco dal loop
-    connection.sandall (b"Letto\n") #risposta al clien con saluto
+    connection.sendall (b"Letto\n") #risposta al clien con saluto
                                     #comando x inviare a tutti un msg a schermo
                                     #di conferma che è arrivato a destinazione
     print(data.decode('utf-8'))     #stampo l'invio del client codificato in utf-8
